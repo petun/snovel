fe95c5c839de92cf80ba3a073cf89aefda0d18b9
petun <petun911@gmail.com>
2015-11-13 08:43:05 +0300

    fIX INDEX page

--- _footer.php
+++ _footer.php
@@ -2,6 +2,59 @@
                           FOOTER
 =========================================================-->
 <footer id="footer">
+	<? if (isset($showSlider)) {?>
+		<section class="fbox-3 wow fadeInUp">
+			<div class="container">
+				<div class="row">
+					<div class="grid_3">
+						<!--=======================-->
+						<div class="fbox-4 text-7 color-4">Navigation</div>
+						<ul class="list-1 text-8">
+							<li><a href="index.html">Home</a></li>
+							<li><a href="index-1.html">About</a></li>
+							<li><a href="index-2.html">What we do</a></li>
+							<li><a href="index-3.html">Folio</a></li>
+							<li><a href="index-4.html">Blog</a></li>
+							<li><a href="index-5.html">Contacts</a></li>
+						</ul>
+						<!--=======================-->
+					</div>
+					<div class="grid_3">
+						<!--=======================-->
+						<div class="fbox-4 text-7 color-4">For members</div>
+						<ul class="list-1 text-8">
+							<li><a href="#">Forums</a></li>
+							<li><a href="#">Promotions</a></li>
+							<li><a href="#">Sign in</a></li>
+							<li><a href="#">News</a></li>
+						</ul>
+						<!--=======================-->
+					</div>
+					<div class="grid_3">
+						<!--=======================-->
+						<div class="fbox-4 text-7 color-4">Follow us</div>
+						<ul class="list-1 text-8">
+							<li><a href="#">Twitter</a></li>
+							<li><a href="#">Facebook</a></li>
+							<li><a href="#">RSS</a></li>
+						</ul>
+						<!--=======================-->
+					</div>
+					<div class="grid_3">
+						<!--=======================-->
+						<div class="fbox-4 text-7 color-4">Our location</div>
+						<div class="fbox-5 text89">
+							<p>9870 ST VINCENT PLACE,<br>GLASGOW, DC 45 FR 45.</p>
+							<p>TELEPHONE: +1 800 603 6035<br>FAX: +1 800 889 9898</p>
+						</div>
+						<!--=======================-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+	<?}?>
 	<section class="fbox-1 text-2 color-1">
 		<div class="container">
 			<div class="row">
